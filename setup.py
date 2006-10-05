384a482a3c26e94cbbda6021beeb6884b33e5441
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2006-10-05 22:51:42 +0000

    commons settings gathered in one place
    git-svn-id: file:///mnt/data/SVNrepo@156 a13746d7-5020-4d46-8679-245a07f888a7

---
+++ setup.py
@@ -0,0 +1,7 @@
+encoding_path	= 'enc/'
+tex_paths	= ['/usr/share/texmf/', '/usr/share/texmf-tetex']
+
+svg_font_path	= 'fonts/'
+cache_path	= 'cache/'
+font_lookup	= 'enc/font.info'
+enc_lookup	= 'enc/enc.info'
