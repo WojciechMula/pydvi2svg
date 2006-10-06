175c4b3910a1063ee57d736d72ede2268984bae6
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2006-10-06 17:56:08 +0000

    - added fontDB functions: 	- load fonts at given scale (create_DVI_font) 	- get glyph data, its scale and hadv for given char (get_char) - ovrride encoding of font
    git-svn-id: file:///mnt/data/SVNrepo@169 a13746d7-5020-4d46-8679-245a07f888a7

--- conv/fontsel.py
+++ conv/fontsel.py
@@ -1,7 +1,8 @@
-# pydvips v0.8
+# pydvi2svg
 # -*- coding: iso-8859-2 -*-
 #
 # SVG font & char encoding utilities
+# $Id: fontsel.py,v 1.5 2006-10-06 17:56:08 wojtek Exp $
 # 
 # license: BSD
 #
@@ -9,9 +10,26 @@
 # e-mail: wojciech_mula@poczta.onet.pl
 
 __changelog__ = '''
-5.10.2006
+ 6.10.2006
+ 	- added fontDB functions:
+		- load fonts at given scale (create_DVI_font)
+		- get glyph data, its scale and hadv for given char (get_char)
+	- ovrride encoding of font
+	- permanently modify encoding (change file setup.font_lookup)
+ 5.10.2006
 	- removed unused function
 	- use logging module
+ 2.10.2006
+    - remove SVGFont class
+	- added following functions:
+		* make_cache_file
+		* get_glyph
+		* get_height
+		* glyph_info
+		* font_info
+		* unknown_fonts
+ 1.10.2006
+ 	- moved font releated functions from main program here
 '''
 
 import sys
@@ -23,12 +41,15 @@ import logging
 import setup
 import findfile
 
-from binfile import binfile
-from metrics import read_TFM, TFMError, read_AFM, AFMError, read_MAP
+from binfile  import binfile
+from metrics  import read_TFM, TFMError, read_AFM, AFMError, read_MAP
+from encoding import EncodingDB, EncodingDBError
 
-logging.basicConfig(level=logging.DEBUG)
+# create encodingDB instance
+encodingDB	= EncodingDB(setup.encoding_path, setup.tex_paths)
 
-log = logging.getLogger('fonts')
+# get logger
+log 		= logging.getLogger('fonts')
 
 class FontError(Exception):
 	pass
@@ -39,18 +60,95 @@ class Config(object):
 config = Config()
 
 # table using to translate long encoding names into shorter form
-config.encoding_lookup = {}
+config.encoding_lookup	= {}
 
-# table using to retrieve trivial info about given font: its
+# dict contains trivial info about fonts: its
 # encoding name (short form) and design size
-config.fonts_lookup    = {}
+config.fonts_lookup		= {}
 
-def init():
-	log.info("Loading encoding names lookup from '%s'" % setup.enc_lookup)
+# loaded fonts
+config.fonts			= {}
+
+# created DVI fonts
+config.dvi_fonts		= {}
+
+def create_DVI_font(fontname, k, s, d):
+	"""
+	Create a font identified by number k scaled with factor s/d.
+	If encoding is None, then use default encoding assigned
+	to fontname.
+	"""
+
+	# load font
+	fontdata = load_font(fontname)
+
+	class DVIFont:
+		pass
+
+	font = DVIFont()
+	font.name			= fontname
+	font.scale			= float(s)/d * fontdata.designsize/1000.0
+	font.hadvscale		= float(s)/1000
+	font.glyphs_dict	= fontdata.glyphs_dict
+	font.encoding		= encodingDB.getencodingtbl(fontdata.encoding)
+
+	config.dvi_fonts[k] = font
+
+def get_char(fontnum, dvicode):
+	"""
+	Returns following data releated to character:
+	- glyph (shape)
+	- shape scale factor needed to fit current font size
+	- width of char in TeX units
+	"""
+	font = config.dvi_fonts[fontnum]
+
+	glyphname = font.encoding[dvicode]
+	try:
+		glyph = font.glyphs_dict[glyphname]
+		return glyph.path, font.scale, glyph.hadv * font.hadvscale
+	except KeyError, e:
+		log.error("%s: missing char '%s'" % (font.name, glyphname))
+		raise e
+
+def preload(enc_repl={}):
+	log.debug("Loading encoding names lookup from '%s'" % setup.enc_lookup)
 	load_enc_lookup()
 	
-	log.info("Loading font info from '%s'" % setup.font_lookup)
+	log.debug("Loading font info from '%s'" % setup.font_lookup)
 	load_font_info()
+	for fontname, newenc in enc_repl.iteritems():
+		if fontname not in config.fonts_lookup:
+			continue
+
+		enc, ds = config.fonts_lookup[fontname]
+		if enc != newenc:
+			log.debug("Encoding of '%s' set to %s", fontname, newenc)
+			config.fonts_lookup[fontname] = (newenc, ds)
+
+
+def load_font(fontname):
+	try:
+		font = config.fonts[fontname]
+		log.debug("Getting font '%s' from cache" % fontname)
+	except KeyError:
+		filename = os.path.join(setup.cache_path + fontname.lower() + '.cache')
+		if not os.path.exists(filename):
+			log.debug("Cache file for '%s' font does not exists -- creating it" % fontname)
+			make_cache_file(fontname)
+			config.fonts[fontname] = font = cPickle.load(open(filename, 'rb'))
+		else:
+			log.debug("Loading font '%s' from cache file" % fontname)
+			config.fonts[fontname] = font = cPickle.load(open(filename, 'rb'))
+
+	if fontname in config.fonts_lookup:
+		if font.encoding != config.fonts_lookup[fontname][0]:
+			newencoding = config.fonts_lookup[fontname][0]
+			log.debug("Overriding font encoding (%s->%s)" % (font.encoding, newencoding))
+			font.encoding = newencoding
+
+	return font
+	
 
 def load_enc_lookup():
 	try:
@@ -98,14 +196,14 @@ def load_font_info():
 			continue
 
 		try:
-			fontname, encodingname, designsize = line.split()
+			fontname, encoding, designsize = line.split()
 			if fontname not in config.fonts_lookup:
-				config.fonts_lookup[fontname] = (encodingname, float(designsize))
+				config.fonts_lookup[fontname] = (encoding, float(designsize))
 				log.debug("... line %d: %s @ %spt, enc. %s" % (
 					i+1,
 					fontname,
 					str(float(designsize)),
-					encodingname,
+					encoding,
 				))
 			else:
 				log.warning("... line %d: font %s - duplicated entry ignored" % (i+1, fontname))
@@ -116,7 +214,6 @@ def load_font_info():
 	
 	file.close()
 
-verbose_level = 0
 
 tex_map_file_list = None
 def get_encoding(fontname, checktfm=True, checkafm=True, grepmap=False):
@@ -149,31 +246,7 @@ def get_encoding(fontname, checktfm=True, checkafm=True, grepmap=False):
 			enc = read_MAP(filename, fontname)
 			if enc:
 				log.debug("... ... ... encoding %s" % enc)
-				return ecn
-	
-	if checkafm:
-		log.debug('... checking AFM file')
-		filename = findfile.locate(fontname + '.afm')
-		if filename:
-			log.debug("... ... using '%s'" % filename)
-			file = open(filename, 'r')
-			try:
-				encodingname, _ = read_AFM(file)
-			except AFMError, e:
-				log.error("... ... AFM error" + str(AFMError))
-			else:
-				file.close()
-				try:
-					if encodingname:
-						encoding = config.encoding_lookup[encodingname]
-						log.debug("... ... ... encoding %s" % encoding)
-						return (True, encoding)
-					else:
-						log.error("... ... unknown AFM encoding: '%s'" % encodingname)
-				except KeyError:
-					log.error("... ... unknown AFM encoding: '%s'" % encodingname)
-		else:
-			log.debug("... ... AFM file not found")
+				return enc
 	
 	if checktfm:
 		log.debug('... checking TFM file')
@@ -195,6 +268,31 @@ def get_encoding(fontname, checktfm=True, checkafm=True, grepmap=False):
 					log.error("... ... font %s: unknown TeX encoding: '%s'" % (fontname, encodingname))
 		else:
 			log.debug("... ... TFM file not found")
+	
+	
+	if checkafm:
+		log.debug('... checking AFM file')
+		filename = findfile.locate(fontname + '.afm')
+		if filename:
+			log.debug("... ... using '%s'" % filename)
+			file = open(filename, 'r')
+			try:
+				encodingname, _, _ = read_AFM(file)
+			except AFMError, e:
+				log.error("... ... AFM error" + str(AFMError))
+			else:
+				file.close()
+				try:
+					if encodingname:
+						encoding = config.encoding_lookup[encodingname]
+						log.debug("... ... ... encoding %s" % encoding)
+						return (True, encoding)
+					else:
+						log.error("... ... unknown AFM encoding: '%s'" % encodingname)
+				except KeyError:
+					log.error("... ... unknown AFM encoding: '%s'" % encodingname)
+		else:
+			log.debug("... ... AFM file not found")
 	
 
 	# encoding not resolved
@@ -234,36 +332,48 @@ def get_designsize(fontname):
 	
 re_number	= re.compile('(\d+)')
 re_mapping	= re.compile("^([0-9a-fA-F]{4}) N (.+)")
+	
+class Font:
+	pass
+
+class Glyph:
+	def __init__(self):
+		self.path    = None
+		self.unicode = u""
+		self.name    = ""
+
 def make_cache_file(fontname):
+
+	font = Font()
+	font.name = fontname
 	
 	# locate SVG
 	filename = findfile.findfile(fontname + '.svg', setup.svg_font_path, ignorecase=True)
 	if filename:
-		log.debug("Using SVG font '%s' as '%s'" % (filename, fontname))
+		log.debug("Using SVG font '%s' as '%s'", filename, font.name)
 	else:
-		raise FontError("Can't find SVG font for '%s'" % fontname)
+		raise FontError("Can't find SVG font for '%s'" % font.name)
 	
 	# get designsize & encoding
-	designsize = get_designsize(fontname)
-	log.debug("Font '%s' designed at %spt" % (fontname, str(designsize)))
-	newfile, encoding = get_encoding(fontname)
-	if encoding == False:
-		raise FontError("Can't determine encoding for font '%s'" % fontname)
+	font.designsize = get_designsize(fontname)
+	log.debug("Font '%s' designed at %spt" % (fontname, str(font.designsize)))
+	newfile, font.encoding = get_encoding(fontname)
+	if font.encoding == None:
+		raise FontError("Can't determine encoding for font '%s'", font.name)
 	else:
-		log.debug("Font '%s' encoding is %s" % (fontname, encoding))
+		log.debug("Font '%s' encoding is %s", font.name, font.encoding)
 	
 	if newfile:
 		log.debug("Adding information about new font to '%s': %s@%fpt, enc. %s" % (
 			setup.font_lookup,
-			fontname,
-			designsize,
-			encoding)
+			font.name,
+			font.designsize,
+			font.encoding)
 		)
 		file = open(setup.font_lookup, 'a')
-		file.write("%s\t\t%s\t\t%s\n" % (fontname, encoding, str(designsize)))
+		file.write("%s\t\t%s\t\t%s\n" % (font.name, font.encoding, str(font.designsize)))
 		file.close()
 	
-
 	# 2. Process SVG file
 	
 	# a. load file
@@ -271,96 +381,67 @@ def make_cache_file(fontname):
 	data = parse(filename)
 
 	# b. get font element
-	font = data.getElementsByTagName('font')
-	if len(font) < 1:
+	try:
+		fontnode = data.getElementsByTagName('font')[0]
+		# get default horizontal advance if any
+		if fontnode.hasAttribute('horiz-adv-x'): 
+			default_hadvx = float(fontnode.getAttribute('horiz-adv-x'))
+		else:
+			default_hadvx = None
+	except IndexError:
 		raise FontError("There should be at least one <font> element in SVG file")
-	
-	if len(font) > 1:
-		# XXX: FontForge do not output more then 1
-		log.debug("There are more then one font definition in the file, using only first")
-	font = font[0]
-	
-	# c. get default horizontal advance if any
-	if font.hasAttribute('horiz-adv-x'): 
-		default_hadvx = float(font.getAttribute('horiz-adv-x'))
-	else:
-		default_hadvx = None
 
 	# d. load fonts
-	glyphs = {}
-	for glyph in font.getElementsByTagName('glyph'):
+	font.glyphs_dict = {}
+	for node in fontnode.getElementsByTagName('glyph'):
 
 		# 1. read path info
-		path = None
-		if glyph.hasAttribute('d'):	# is defined as <glyph> attribute
-			path = glyph.getAttribute('d')
+		glyph = Glyph()
+		if node.hasAttribute('d'):	# is defined as <glyph> attribute
+			glyph.path = node.getAttribute('d')
 		else:
 			# XXX: glyph is defined using childNodes and I assume
 			#      there is just one path element
-			path_elements = glyph.getElementsByTagName('path')
+			path_elements = node.getElementsByTagName('path')
 			if len(path_elements) == 0:		# no path elements at all
 				pass
 			elif len(path_elements) == 1:	# one path element
-				path = path_elements[0].getAttribute('d')
+				glyph.path = path_elements[0].getAttribute('d')
 			else: # more paths
 				pass # XXX: join them?
-
-		# 2. get horiz-adv-x
-		if glyph.hasAttribute('horiz-adv-x'):
-			hadvx = float(glyph.getAttribute('horiz-adv-x'))
+		
+		# 2. get character name
+		glyph.name = node.getAttribute('glyph-name')
+		if  glyph.name == '':
+			log.error("There is a glyph without name, skipping it")
+			continue
+		elif glyph.name == "space":
+			glyph.unicode = u" "
 		else:
-			try:
-				hadvx = default_hadvx
-			except NameError:
-				# TODO: calculate glyphs extends
-				raise NotImplementedError
+			glyph.unicode = node.getAttribute('unicode')
 
-		name = glyph.getAttribute('glyph-name')
-		if name == "space":
-			unicode = u" "
+		# 3. get horiz-adv-x
+		if node.hasAttribute('horiz-adv-x'):
+			glyph.hadv = float(node.getAttribute('horiz-adv-x'))
+		elif default_hadvx != None:
+			glyph.hadv = default_hadvx
 		else:
-			unicode = glyph.getAttribute('unicode')
+			# XXX: calculate glyphs extends?
+			raise FontError("Can't determine width of character '%s'", glyph.name)
 
-		#for dvicode in encoding[name]:
-		#	glyphs[dvicode] = (dvicode, unicode, name, hadvx, path)
-	
-		if name in glyphs:
-			raise ValueError("???")
-		glyphs[name] = (unicode, hadvx, path)
+		if glyph.name in font.glyphs_dict:
+			log.error("Character '%s' already defined, skipping", glyph.name)
+		else:
+			font.glyphs_dict[glyph.name] = glyph
+	#rof
 
 	# e. write the cache file
-	font_info  = (fontname, designsize)
-	font = (encoding, font_info, glyphs)
 	cPickle.dump(font, open(setup.cache_path + fontname + '.cache', 'wb'), protocol=cPickle.HIGHEST_PROTOCOL)
-
-font_cache = {}
-
-def get_font(fontname, recache):
-	try:
-		font = font_cache[fontname]
-		log.debug("Getting font '%s' from cache" % fontname)
-	except KeyError:
-		filename = os.path.join(setup.cache_path + fontname.lower() + '.cache')
-		if recache:
-			log.debug("Forced to recreate cache file for '%s' font" % fontname)
-			make_cache_file(fontname)
-			font_cache[fontname] = font = cPickle.load(open(filename, 'rb'))
-		elif not os.path.exists(filename):
-			log.debug("Cache file for '%s' font does not exists -- creating it" % fontname)
-			make_cache_file(fontname)
-			font_cache[fontname] = font = cPickle.load(open(filename, 'rb'))
-		else:
-			log.debug("Loading font '%s' from cache file" % fontname)
-			font_cache[fontname] = font = cPickle.load(open(filename, 'rb'))
-	
-	return font
 
 def is_font_supported(fontname):
 	return findfile.findfile(fontname + ".svg", setup.svg_font_path, ignorecase=True) != None
 	
 def unavailable_fonts(fontslist):
 	return [fontname for fontname in fontslist if not is_font_supported(fontname)]
-
-init()
 
 # vim: ts=4 sw=4
