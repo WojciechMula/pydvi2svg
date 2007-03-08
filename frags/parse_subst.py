f2baa5095ccc432b4f21471b42c1c26c46245787
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-03-08 21:24:58 +0000

    *** empty log message ***
    git-svn-id: file:///mnt/data/SVNrepo@686 a13746d7-5020-4d46-8679-245a07f888a7

--- frags/parse_subst.py
+++ frags/parse_subst.py
@@ -1,9 +1,10 @@
 """
-"string"|#id|rect(x,y,w,h) -> "TeX"
+"string"|#id|rect(x,y,w,h)|point(x,y) -> "TeX"
 	position: x|center/c|left/l|right/r, y|center/c|top/t|bottom/b|
 	settowidth: number|rectid|this
 	settoheight: number|rectid|this
 	fit
+	scale: number
 """
 
 import re
@@ -12,6 +13,7 @@ re_quoted_string = re.compile(r'\s*"((?:\\"|[^"])*)"')
 re_id     = re.compile(r'\s*(#[a-zA-Z0-9_-]+)')
 re_number = re.compile(r'\s*([+-]?\d*\.\d+|[+-]?\d+\.\d*|[-+]?\d+)')
 re_rect   = re.compile(r'\s*rect\s*\(', re.IGNORECASE)
+re_point  = re.compile(r'\s*point\s*\(', re.IGNORECASE)
 re_closingbrace  = re.compile(r'\s*\)')
 re_comma  = re.compile(r'\s*,\s*')
 re_colon  = re.compile(r'\s*:\s*')
@@ -23,12 +25,14 @@ def tokenize(string):
 	class Dummy: pass
 	s   = Dummy()
 	s.s = string
+	s.re_cache = {}
 
 	def consume(regexp, match_needed=""):
 		m = regexp.match(s.s)
 		if not m:
 			if match_needed:
-				raise ValueError(str(match_needed))
+				tmp = "%s...\n%s" % (s.s[:20], str(match_needed))
+				raise ValueError(tmp)
 			else:
 				return None
 
@@ -39,9 +43,15 @@ def tokenize(string):
 			return True
 	
 	def consume_string(string, match_needed=False):
-		regexp = re.compile('('+string+')', re.IGNORECASE)
+		try:
+			regexp = s.re_cache[string]
+		except KeyError:
+			regexp = re.compile('('+string+')', re.IGNORECASE)
+			s.re_cache[string] = regexp
+		
 		consume(re_spaces)
 		return consume(regexp, match_needed)
+
 	
 	def notNone(value, info="None not accpeted!"):
 		if value is None:
@@ -55,15 +65,17 @@ def tokenize(string):
 			break
 
 		A = consume(re_quoted_string) or consume(re_id)
-		if A is None:
-			consume(re_rect, "rect expeced")
-
+		if A is None and consume(re_rect):
 			x = consume(re_number, "number 'x'"); consume(re_comma, "comma expeced")
 			y = consume(re_number, "number 'y'"); consume(re_comma, "comma expeced")
 			w = consume(re_number, "number 'width'"); consume(re_comma, "comma expeced")
 			h = consume(re_number, "number 'height'")
 			A = (x, y, w, h)
-
+			consume(re_closingbrace, "you forgot to close brace")
+		if A is None and consume(re_point):
+			x = consume(re_number, "number 'x'"); consume(re_comma, "comma expeced")
+			y = consume(re_number, "number 'y'")
+			A = (x, y)
 			consume(re_closingbrace, "you forgot to close brace")
 
 		consume(re_spaces)
@@ -114,16 +126,21 @@ def tokenize(string):
 			notNone(fh, "Dupa")
 	
 		fit = consume_string('fit')
+
+		scale = consume_string('scale')
+		if scale and consume(re_colon, "colon is needed after keyword 'scale'"):
+			scale = consume(re_number, "scale factor needed")
 			
 				
-		print A, tex, px, py, fw, fh, fit
+		print A, tex, px, py, fw, fh, fit, scale
 
 
 
 sample = """
+point(10, -.5) = "This is sample text" position:0.2,bottom
 "LaTeX" -> "\\LaTeX" position: left, 0.57 settoheight: this
 "emc2"  -> "$e = mc^2$" settowidth :  #rect02-a
-  rect  (   10, 5.5, 100, 200.5  ) -> "$\\fract{1}{x^2 + 1}" position: right, 0.2 fit
+  rect  (   10, 5.5, 100, 200.5  ) -> "$\\fract{1}{x^2 + 1}$" position: right, 0.2 fit
 """
 tokenize(sample)
 
