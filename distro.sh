9926e734d2c47e0414de4b182865e0cf974eaafb
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-03-13 21:02:23 +0000

    updates
    git-svn-id: file:///mnt/data/SVNrepo@760 a13746d7-5020-4d46-8679-245a07f888a7

--- distro.sh
+++ distro.sh
@@ -2,6 +2,6 @@
 
 d=pydvi2svg
 cd ..
-tar --exclude=$d/enc/CVS --exclude=$d/cache/* -cjf $d/$d.tar.bz2 $d/*.py $d/enc $d/cache
+tar --exclude=$d/enc/CVS --exclude=$d/cache/* -cjf $d/$d.tar.bz2 $d/*.py $d/enc $d/cache $d/conv/*.py $d/frags/*.py
 #tar -cjf $d/fonts.tar.bz2 $d/fonts/cm/*
-tar --exclude=$d/enc/CVS --exclude=$d/cache/* -cjf $d/$d-full.tar.bz2 $d/*.py $d/enc $d/cache $d/fonts/cm/*
+#tar --exclude=$d/enc/CVS --exclude=$d/cache/* -cjf $d/$d-full.tar.bz2 $d/*.py $d/enc $d/cache $d/fonts/cm/*
