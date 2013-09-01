647a7aa2f60f01f8bdb9a2875e91b293e8a3656f
Wojciech Muła <wm@ack>
2013-09-01 16:44:26 +0200

    removed unused files

--- distro.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-d=pydvi2svg
-cd ..
-tar --exclude=$d/enc/CVS --exclude=$d/cache/* -cjf $d/$d.tar.bz2 $d/*.py $d/enc $d/cache $d/conv/*.py $d/frags/*.py
-#tar -cjf $d/fonts.tar.bz2 $d/fonts/cm/*
-#tar --exclude=$d/enc/CVS --exclude=$d/cache/* -cjf $d/$d-full.tar.bz2 $d/*.py $d/enc $d/cache $d/fonts/cm/*
